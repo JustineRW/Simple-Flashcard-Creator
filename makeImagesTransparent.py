ce14f9416d73ff4450c6e55ccd9700e8c4443602
JustineRW <waterson.justine@gmail.com>
2025-10-12 13:42:21 +1300

    Make transparent image script into function

--- makeImagesTransparent.py
+++ makeImagesTransparent.py
@@ -3,11 +3,15 @@ import pathlib
 import pandas as pd
 
 df = pd.read_csv('database.csv')
+filePath = "images/back/"
 
-for index, row in df.iterrows():
-    fileNameIncludingTransparent = "images/back/" + "transparent " + str.split(row['imageBack'],".")[0] + ".png"
-    if not pathlib.Path(fileNameIncludingTransparent).is_file():        
-        backImage = Image.open(pathlib.Path("images/back/" + row['imageBack'] )) 
+def make_images_transparent(filePath, imageName):
+    resultFileName = filePath + "transparent " + str.split(imageName,".")[0] + ".png"
+    if not pathlib.Path(resultFileName).is_file():        
+        backImage = Image.open(pathlib.Path(filePath + imageName)) 
         backImage = backImage.convert("RGBA")
         backImage.putalpha(63)  # quarter alpha; alpha argument must be an int
-        backImage.save(pathlib.Path(fileNameIncludingTransparent))
+        backImage.save(pathlib.Path(resultFileName))
+
+for index, row in df.iterrows():
+    make_images_transparent(filePath, row['imageBack'])
